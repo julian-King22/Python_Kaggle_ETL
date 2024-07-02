8a3e228c10aedd3c68c84400e651b9471fc9d752
AviatorIfeanyi <inboxlegacy@gmail.com>
2024-07-02 01:43:40 +0100

    extract file contents and load to folder

--- extract.py
+++ extract.py
@@ -21,3 +21,23 @@ def download_file(download_dir):
         os.system(f'kaggle datasets download ankitbansal06/retail-orders -f orders.csv -p {download_dir}')
 
     return zip_file_path, download_dir
+
+def extract_file(zip_file_path, download_dir):
+    dataset_dir = "data"
+    try:
+        with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+            zip_ref.extractall(f'{dataset_dir}/orders.zip')
+            print(f"Downloaded and extracted to: {dataset_dir}")
+    except:
+        print("Failed to download the file.")
+    finally:
+        shutil.rmtree(f"{download_dir}")
+
+
+
+if __name__ == '__main__':
+data_folder = create_download_dir()
+
+file_path, download_dir = download_file(data_folder)
+
+extract_file(file_path,download_dir)
