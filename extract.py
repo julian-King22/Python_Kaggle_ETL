072b240005ac90cbc0fd1d34dbcf33be2827e27c
AviatorIfeanyi <inboxlegacy@gmail.com>
2024-07-02 01:37:06 +0100

    download file from source

--- extract.py
+++ extract.py
@@ -11,3 +11,13 @@ def create_download_dir():
     
     return download_dir
 
+
+def download_file(download_dir):
+    zip_file_path = os.path.join(f"{download_dir}", 'orders.csv.zip')
+
+    if os.path.exists(zip_file_path):
+        print('file already exists')
+    else:
+        os.system(f'kaggle datasets download ankitbansal06/retail-orders -f orders.csv -p {download_dir}')
+
+    return zip_file_path, download_dir
